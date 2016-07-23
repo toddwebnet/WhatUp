51678589114d95e0e4d863358a5ba96691dfb697
James Todd <toddwebnet@gmail.com>
2016-07-23 11:08:23 -0500

    updates

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 define ("DBHOST", "127.0.0.1");
 define ("DBNAME", "whatup");
 define ("DBUSERNAME", "root");
-define ("DBPASSWORD", "root");
+define ("DBPASSWORD", "");
 define ("MYSQL_BIN_PATH", "/Applications/MAMP/Library/bin/mysql");
 define ("DB_FILE_PATH", __DIR__ . "/db/imports/");
--- testSites.php
+++ testSites.php
@@ -1,12 +1,13 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
 
-require_once("config.php");
-require_once "includes/helpers.php";
-require_once "includes/mysql.class.php";
-require_once "includes/whatupdb.class.php";
-require_once "includes/testing.class.php";
-require_once "includes/randomizer.class.php";
+
+require_once __DIR__ . "/config.php";
+require_once __DIR__ . "/includes/helpers.php";
+require_once __DIR__ . "/includes/mysql.class.php";
+require_once __DIR__ . "/includes/whatupdb.class.php";
+require_once __DIR__ . "/includes/testing.class.php";
+require_once __DIR__ . "/includes/randomizer.class.php";
 
 $obj = new Testing(new Mysql());
 //$obj = new Randomizer(new Mysql());
