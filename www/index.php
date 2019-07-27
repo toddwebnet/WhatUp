bc3eea74607d2502914c4fa9243614cdd63ae80c
James Todd <james.todd@emailonacid.com>
2019-07-26 21:21:39 -0500

    updates

--- www/index.php
+++ www/index.php
@@ -1,8 +1,8 @@
 <html>
 <head>
-    <script src="/assets/chart.js/dist/Chart.bundle.js"></script>
-    <script src="/assets/js/jquery.js"></script>
-    <script src="/assets/js/app.js"></script>
+    <script src="assets/chart.js/dist/Chart.bundle.js"></script>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/app.js"></script>
     <style>
         canvas {
             -moz-user-select: none;
@@ -17,13 +17,13 @@
     <table style="width: 100%">
         <tr>
             <td style="vertical-align: top; text-align: center; width: 33%" id="upTimeHourly">
-                <img src="/assets/images/loading.gif" style="width:75px"/>
+                <img src="assets/images/loading.gif" style="width:75px"/>
             </td>
             <td style="vertical-align: top; text-align: center; width: 33%" id="upTimeWeekly">
-                <img src="/assets/images/loading.gif" style="width:75px"/>
+                <img src="assets/images/loading.gif" style="width:75px"/>
             </td>
             <td style="vertical-align: top; text-align: center; width: 33%" id="upTimeMonthly">
-                <img src="/assets/images/loading.gif" style="width:75px"/>
+                <img src="assets/images/loading.gif" style="width:75px"/>
             </td>
         </tr>
     </table>
